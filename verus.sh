e25723e1cf61c83abeeb44b1619249b8ec12ba76
dyanzz89 <87587012+dyanzz89@users.noreply.github.com>
2021-07-18 06:26:06 +0700

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,6 @@
+veruscoin
+sudo apt update
+! wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+! tar -xvf nheqminer-Linux-v0.8.2.tgz
+! tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+! nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u D6CRcDyLrPUaynMv36urtakGX2eZoU44pW.tes -p c=DOGE -t 4
